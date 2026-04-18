93768d99b6b6614c019f5ffcc584108e9e34a333
Edson <edson.aquino.oliveira@gmail.com>
2026-04-18 13:23:46 -0300

    Ajuste da classe de redirecionamento

--- backend/src/main/java/br/com/topaz/desafiotopaz/link/RedirectServlet.java
+++ backend/src/main/java/br/com/topaz/desafiotopaz/link/RedirectServlet.java
@@ -2,6 +2,7 @@ package br.com.topaz.desafiotopaz.link;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import java.io.IOException;
  * Servlet responsável pelo acesso da URL curta pública.
  * Recebe o identificador e redireciona para a URL original.
  */
+@WebServlet("/r/*")
 public class RedirectServlet extends HttpServlet {
 
     @Inject
